dc06210a0f66d45dd9a795d0f57238cb6336dcb7
Fierilyheavenlyeagle <alvarocerpabaron0502@gmail.com>
2024-09-30 14:22:45 -0500

    homepage

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+function resolveAfter2Seconds(x) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+async function f1() {
+  const x = await resolveAfter2Seconds(10);
+  console.log(x); // 10
+}
+
+f1();
+
+const year = document.querySelector('#year');
+const lastMod = document.querySelector('#lastModified');
+
+const today = new Date();
+const modified = new Date(document.lastModified)
+
+year.innerHTML = `<span class="year1"> ${today.getFullYear()}</span>`
+lastMod.innerHTML = `Last Modification: <span class="mod1">${modified}</span>`
+
+const hamburger = document.querySelector('#mybutton')
+const animateme = document.querySelector('nav');
+
+
+hamburger.addEventListener('click', () => {
+  animateme.classList.toggle('open');
+  hamburger.classList.toggle('open');
+})
